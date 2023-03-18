0151629f25c71c466a7a381e4bfd48a4ee9f3f4b
Roxie2003 <ruchita03wagh@gmail.com>
2023-03-19 01:56:41 +0530

    experiement2.14

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
-
+const mongooseConnection = require("./helpers/mongodb-connection");
 const PORT = 3000;
 const allEmployeeRoutes = require("./routes/allEmployees");
-const employeeRoutes = require("./routes/employee");
-const attendanceRoutes = require("./routes/attendance");
+
 const cors = require("cors");
 var path = require("path");
 var serveStatic = require("serve-static");
-
+mongooseConnection();
 app.use(serveStatic(path.join(__dirname, "public-optimized")));
 app.use(serveStatic(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/api/employees", allEmployeeRoutes);
-app.use("/api/employee", employeeRoutes);
-app.use("/api/employee/attendance", attendanceRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("HELLO...");
