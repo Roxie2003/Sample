a2d6e578dda6213d0a88d9d0c2e43fc471540453
Roxie2003 <ruchita03wagh@gmail.com>
2023-03-19 02:20:01 +0530

    experiement2.22

--- helpers/db.js
+++ helpers/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://KapilMundada:Kapil123@atlascluster.beoc0fg.mongodb.net/Employee_Payroll?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
