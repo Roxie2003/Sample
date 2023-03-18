d1920a08b78bdfaa3b14b6a6ce975dbc29acac44
Roxie2003 <ruchita03wagh@gmail.com>
2023-03-19 01:53:57 +0530

    experiement2.13

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const mongooseConnection = require("./helpers/mongodb-connection");
+
+const PORT = 3000;
 const allEmployeeRoutes = require("./routes/allEmployees");
-const PORT = 8000;
-
-mongooseConnection();
-app.use(bodyParser.json());
-
+const employeeRoutes = require("./routes/employee");
+const attendanceRoutes = require("./routes/attendance");
 const cors = require("cors");
-const path = require("path");
-const serveStatic = require("serve-static");
+var path = require("path");
+var serveStatic = require("serve-static");
 
 app.use(serveStatic(path.join(__dirname, "public-optimized")));
 app.use(serveStatic(path.join(__dirname, "public")));
 app.use(cors());
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/employees", allEmployeeRoutes);
+app.use("/api/employee", employeeRoutes);
+app.use("/api/employee/attendance", attendanceRoutes);
+
 app.get("/", (req, res) => {
   res.status(200).send("HELLO...");
 });
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Live on http://localhost:${PORT}`));
